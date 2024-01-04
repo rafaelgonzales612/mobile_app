7340a17c71c0107f154128cf27bcb53995c39f52
Rafael <rafgonz1222@gmail.com>
2024-01-04 08:04:15 -0600

    update code

--- index.js
+++ index.js
@@ -11,9 +11,23 @@ const shoppingListInDB = ref(database, "shoppingList")
 
 const inputFieldEl = document.getElementById("input-field")
 const addBtnEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 
 addBtnEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     push(shoppingListInDB, inputValue)
     console.log(`${inputValue} added to database.`)
+
+    clearInputFieldEl()
+    appendItemToShoppingListEl(inputValue)
+    // inputFieldEl.value = ""
+    // shoppingListEl.innerHTML += `<li>${inputValue}</li>`
 })
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToShoppingListEl(itemValue) {
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
+}
